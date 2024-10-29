7347dd71abe2bad59769dd4bc5689ac87f82c979
Kamuri Amorim <myk.gata14@gmail.com>
2024-10-29 15:37:03 -0300

    feat: copy old backup

--- job.sh
+++ job.sh
@@ -6,5 +6,17 @@ set -a
 source $ROOT_DIR/.env
 set +a
 
+if [ -f "$FOLDER/backup.sql" ]; then
+    mv $FOLDER/backup.sql $FOLDER/backup.sql.old
+fi
+
+if [ -f "$FOLDER/trilium.tar.gz" ]; then
+    mv $FOLDER/trilium.tar.gz $FOLDER/trilium.tar.gz.old
+fi
+
+if [ -f "$FOLDER/k3s.tar.gz" ]; then
+    mv $FOLDER/k3s.tar.gz $FOLDER/k3s.tar.gz.old
+fi
+
 sh $ROOT_DIR/backup.sh $CP_IP ubuntu ~/.ssh/aws $BACKUP_DIR postgres
 sh $ROOT_DIR/backup.sh $WORKER_IP ubuntu ~/.ssh/aws $BACKUP_DIR trilium
